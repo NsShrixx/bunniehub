459c0f15d3fec723b960c4c5067fd121ca1c7527
NsShrixx <jayjaylaptop1102@outlook.com>
2025-03-24 20:13:34 +0100

    Create hub.lua

---
+++ hub.lua
@@ -0,0 +1,72 @@
+local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt"))()
+
+local win = DiscordLib:Window("🤍Bunnie Hub🤍")
+
+-- Main server
+local mainServ = win:Server("Main", "http://www.roblox.com/asset/?id=6031075938")
+
+-- Arsenal Scripts Channel
+local arsenalScripts = mainServ:Channel("Arsenal Scripts")
+arsenalScripts:Label("QUOTOS HUB")
+arsenalScripts:Button("Execute QUOTOS HUB", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Insertl/QuotasHub/main/BETAv1.3"))()
+end)
+
+-- Rivals Scripts Channel
+local rivalsChannel = mainServ:Channel("Rivals Scripts")
+rivalsChannel:Label("Rivals Script")
+rivalsChannel:Button("Execute Rivals Script", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))()
+end)
+
+-- Blade Ball Channel
+local bladeBallChannel = mainServ:Channel("Blade Ball")
+local autoParryEnabled = false
+bladeBallChannel:Toggle("Enable Auto Parry", false, function(bool)
+    autoParryEnabled = bool
+end)
+
+-- Auto Parry Logic
+local function autoParry()
+    while autoParryEnabled do
+        -- Add logic for Auto Parry
+        wait(0.1)
+    end
+end
+
+-- Visuals Channel with Aimbot & FOV
+local visualChannel = mainServ:Channel("Visual")
+_G.AimbotEnabled = false
+_G.CircleRadius = 80
+
+visualChannel:Toggle("Enable Aimbot", false, function(bool)
+    _G.AimbotEnabled = bool
+end)
+
+visualChannel:Slider("FOV Radius", 50, 200, _G.CircleRadius, function(val)
+    _G.CircleRadius = val
+end)
+
+-- Misc Server
+local miscServ = win:Server("Misc", "http://www.roblox.com/asset/?id=6031075938")
+local adminChannel = miscServ:Channel("Admin")
+adminChannel:Button("Execute Infinite Yield", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end)
+adminChannel:Button("Execute Dex Explorer", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BigBoyTimme/New.Loadstring.Scripts/refs/heads/main/Dex.Explorer"))()
+end)
+
+-- Additional Game Categories
+local games = {
+    "Blox Fruits", "Pet Simulator X", "Tower of Hell", "Brookhaven RP", "Murder Mystery 2", "Adopt Me", "Doors", "Jailbreak", "BedWars"
+}
+
+for _, gameName in ipairs(games) do
+    local gameServer = win:Server(gameName, "http://www.roblox.com/asset/?id=6031075938")
+    local gameChannel = gameServer:Channel(gameName .. " Scripts")
+    gameChannel:Label("Insert script link below")
+    gameChannel:Textbox("Script URL", "Paste here!", false, function(link)
+        loadstring(game:HttpGet(link))()
+    end)
+end
