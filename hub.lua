8d6357800c97dcd03504a84d1f318a9c2f301092
NsShrixx <jayjaylaptop1102@outlook.com>
2025-03-25 19:14:28 +0100

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,88 +1,153 @@
-local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt"))()
+-- Load the required libraries
 
-local win = DiscordLib:Window("🤍Bunnie Hub🤍")
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/NsShrixx/bunniehub/refs/heads/main/sigmaboy.lua"))()
 
--- Main server
-local mainServ = win:Server("Main", "http://www.roblox.com/asset/?id=127668012498415")
-
--- Arsenal Scripts Channel
-local arsenalScripts = mainServ:Channel("Arsenal Scripts")
-arsenalScripts:Label("QUOTOS HUB")
-arsenalScripts:Button("Execute QUOTOS HUB", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Insertl/QuotasHub/main/BETAv1.3"))()
-end)
-
--- Rivals Scripts Channel
-local rivalsChannel = mainServ:Channel("Rivals Scripts")
-rivalsChannel:Label("Rivals Script")
-rivalsChannel:Button("Execute Rivals Script", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))()
-end)
-
--- Blade Ball Channel
-local bladeBallChannel = mainServ:Channel("Blade Ball")
-local autoParryEnabled = false
-bladeBallChannel:Toggle("Enable Auto Parry", false, function(bool)
-    autoParryEnabled = bool
-end)
-
--- Auto Parry Logic
-local function autoParry()
-    while autoParryEnabled do
-        -- Add logic for Auto Parry
-        wait(0.1)
-    end
-end
-
--- Visuals Channel with Aimbot & FOV
-local visualChannel = mainServ:Channel("Visual")
-_G.AimbotEnabled = false
-_G.CircleRadius = 80
-
-visualChannel:Toggle("Enable Aimbot", false, function(bool)
-    _G.AimbotEnabled = bool
-end)
-
-visualChannel:Slider("FOV Radius", 50, 200, _G.CircleRadius, function(val)
-    _G.CircleRadius = val
-end)
-
--- Misc Server
-local miscServ = win:Server("Misc", "http://www.roblox.com/asset/?id=127668012498415")
-local adminChannel = miscServ:Channel("Admin")
-adminChannel:Button("Execute Infinite Yield", function()
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
-end)
-adminChannel:Button("Execute Dex Explorer", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BigBoyTimme/New.Loadstring.Scripts/refs/heads/main/Dex.Explorer"))()
-end)
-
--- Games Server
-local gamesServ = win:Server("Games", "http://www.roblox.com/asset/?id=127668012498415")
-local games = {
-    "Blox Fruits", "Pet Simulator X", "Tower of Hell", "Brookhaven RP", "Murder Mystery 2", "Adopt Me", "Doors", "Jailbreak", "BedWars"
+-- Create the main GUI
+local GUI = Mercury:Create{
+    Name = "Bunnie hub V1.2",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
 }
 
-for _, gameName in ipairs(games) do
-    local gameChannel = gamesServ:Channel(gameName)
-    gameChannel:Button("Execute Script", function()
-        print("sigma")
-    end)
-end
+-- Asset ID for all tabs' icons
+local commonIcon = "https://www.roblox.com/asset/?id=8569322835"  -- This is the common icon for all tabs
 
--- Settings Server
-local settingsServ = win:Server("Settings", "http://www.roblox.com/asset/?id=127668012498415")
-local settingsChannel = settingsServ:Channel("UI Themes")
+-- Create Tabs with the same icon
+local GamesTab = GUI:Tab{
+    Name = "Games",
+    Icon = commonIcon
+}
 
-settingsChannel:Button("Light Theme", function()
-    DiscordLib:ChangeTheme("Light")
-end)
-settingsChannel:Button("Dark Theme", function()
-    DiscordLib:ChangeTheme("Dark")
-end)
-settingsChannel:Button("Midnight", function()
-    DiscordLib:ChangeTheme("Midnight")
-end)
-settingsChannel:Button("Red Theme", function()
-    DiscordLib:ChangeTheme("Red")
-end)
+local PlayerTab = GUI:Tab{
+    Name = "Player",
+    Icon = commonIcon
+}
+
+local MiscTab = GUI:Tab{
+    Name = "Misc",
+    Icon = commonIcon
+}
+
+-- Games Channels
+local PopularGames = GamesTab:Section{
+    Name = "Popular Games",
+    Side = "Left"
+}
+
+local Exploits = GamesTab:Section{
+    Name = "Game Exploits",
+    Side = "Right"
+}
+
+-- Player Channels
+local Movement = PlayerTab:Section{
+    Name = "Movement",
+    Side = "Left"
+}
+
+local Modifiers = PlayerTab:Section{
+    Name = "Modifiers",
+    Side = "Right"
+}
+
+-- Misc Channels
+local FunStuff = MiscTab:Section{
+    Name = "Fun Stuff",
+    Side = "Left"
+}
+
+local Utility = MiscTab:Section{
+    Name = "Utility",
+    Side = "Right"
+}
+
+-- Games Buttons
+PopularGames:Button{
+    Name = "Teleport to Brookhaven",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(100, 50, 200) -- Example
+    end
+}
+
+PopularGames:Button{
+    Name = "Teleport to Blox Fruits",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(300, 50, 600) -- Example
+    end
+}
+
+-- Player Movement Options
+Movement:Slider{
+    Name = "Walkspeed",
+    Min = 16,
+    Max = 200,
+    Default = 16,
+    Callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    end
+}
+
+Movement:Slider{
+    Name = "Jump Power",
+    Min = 50,
+    Max = 500,
+    Default = 50,
+    Callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+    end
+}
+
+Modifiers:Toggle{
+    Name = "Infinite Jump",
+    StartingState = false,
+    Callback = function(state)
+        if state then
+            game:GetService("UserInputService").JumpRequest:Connect(function()
+                game.Players.LocalPlayer.Character.Humanoid:ChangeState("Jumping")
+            end)
+        end
+    end
+}
+
+-- Miscellaneous Features
+FunStuff:Button{
+    Name = "Spam Chat Message",
+    Callback = function()
+        while true do
+            wait(1)
+            game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hello World!", "All")
+        end
+    end
+}
+
+-- Utility Functions
+Utility:Button{
+    Name = "Server Hop",
+    Callback = function()
+        local HttpService = game:GetService("HttpService")
+        local TPS = game:GetService("TeleportService")
+        local Servers = HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Desc&limit=100"))
+
+        for _, v in pairs(Servers.data) do
+            if v.playing < v.maxPlayers then
+                TPS:TeleportToPlaceInstance(game.PlaceId, v.id, game.Players.LocalPlayer)
+                break
+            end
+        end
+    end
+}
+
+Utility:Button{
+    Name = "Rejoin Server",
+    Callback = function()
+        game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+    end
+}
+
+-- Notification Example
+GUI:Notification{
+    Title = "Welcome to Bunnie Hub!",
+    Text = "Enjoy your Time (We have ur passwords)!",
+    Duration = 7
+}
